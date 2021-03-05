e97eba9a88d30113b85ba912f2a66ed2857cf869
Octavio Rossell <octavio.rossell@gmail.com>
2021-03-05 17:57:11 -0400

    Añadido eventos para control de volumen Rueda del mouse y botones + / -

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,10 @@
 var intervalo;
 var audioplayer = document.getElementById("reproductor");
 
+var vol;
+
+audioplayer.volume = 0.7;
+
 function actualizaNombre() {
     intervalo = setInterval(leeNombre, 5000);
     }
@@ -42,3 +46,35 @@ $('input[type=range]').on('input', function () {
 	});
 
 
+$('#idvol').on('wheel', function(event) {
+  	if(event.originalEvent.deltaY < 0) {
+		if (audioplayer.volume < 1) {
+			vol = audioplayer.volume + 0.1;
+			audioplayer.volume = vol;
+			document.getElementById("rango").value = vol;
+			}
+  		}
+  	else {
+		if (audioplayer.volume > 0) {
+			vol = audioplayer.volume - 0.1;
+	        audioplayer.volume = vol;
+			document.getElementById("rango").value = vol;
+			}
+  		}
+	});
+
+$("#menos").click(function(){
+	if (audioplayer.volume > 0) {
+		vol = audioplayer.volume - 0.1;
+		audioplayer.volume = vol;
+		document.getElementById("rango").value = vol;
+		}
+	})
+
+$("#mas").click(function(){
+    if (audioplayer.volume < 1) {
+        vol = audioplayer.volume + 0.1;
+        audioplayer.volume = vol;
+        document.getElementById("rango").value = vol;
+        }
+    })
